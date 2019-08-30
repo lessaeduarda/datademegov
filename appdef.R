327269216fc346de3010d4ba836899d0f6fa122b
lessaeduarda <49791303+lessaeduarda@users.noreply.github.com>
2019-08-30 16:39:11 -0300

    Interactive Plots - Shiny App

--- appdef.R
+++ appdef.R
@@ -7,14 +7,34 @@ if(require(dplyr)==F)install.packages('dplyr');require(dplyr)
 if(require(plotly)==F)install.packages('plotly');require(plotly)
 if(require(shinythemes)==F)install.packages('shinythemes');require(shinythemes)
 
-# !!! Set your own working directory and load database !!!
-setwd("C:/Users/Duda/Desktop/TrabalhoFinalAD")
+# !! Set your own working directory !! 
+setwd()
+
+# Load database:
 load("mariaeduarda-lessa-ad-ufpe-2019.Rda")
+
+# Set levels for categorical variables:
+baseDemEgov$Continent <- factor(baseDemEgov$Continent, levels = c("Africa",
+                                                                  "America",
+                                                                  "Asia",
+                                                                  "Europe",
+                                                                  "Oceania"))
+
+baseDemEgov$EGDILEVEL <- factor(baseDemEgov$EGDILEVEL, levels = c("Very High EGDI",
+                                                                  "High EGDI",
+                                                                  "Middle EGDI",
+                                                                  "Low EGDI"))
+
+baseDemEgov$DemLevel <- factor(baseDemEgov$DemLevel, levels = c("Full democracy",
+                                                                "Flawed democracy",
+                                                                "Hybrid regime",
+                                                                "Authoritarian"))
+---
 
 shinyApp(
   ui <- shinyUI(fluidPage(
     fluidPage(theme = shinytheme("lumen"),
-      titlePanel("Does Democracy Encourage E-Government?"),
+      titlePanel("Does Democracy Encourage e-Government?"),
       h4( "Data visualization for the relation between The Economist Intelligence Unit's Democracy Index and United Nation's E-Government Development Index, controlled by GDP per capita transformed in log."),
       sidebarLayout(
         sidebarPanel(
@@ -50,23 +70,28 @@ shinyApp(
 
       output$plot1 <- renderPlotly({
         p <- ggplot(data = baseDemEgov, aes(y = EGDI, x = DemIndex + GDPlog, 
-                                            color = EGDILEVEL,label = Country)) +
+                                            color = Continent,label = Country)) +
           theme_replace()
         p <- p + geom_point()+
-          scale_color_manual(values = c("Very High EGDI" = "#4682B4", 
-                                          "High EGDI" = "#3CB371", 
-                                        "Middle EGDI" = "#DAA520", "Low EGDI" = "#E9967A"))
+          scale_color_manual(values = c("Africa" = "#7fcdbb",
+                                        "America" = "#41b6c4",
+                                        "Asia" = "#1d91c0", 
+                                        "Europe" = "#225ea8",
+                                        "Oceania" = "#0c2c84"))
+        
         
         ggplotly(p)
       })
       output$plot2 <- renderPlotly({
         q <- ggplot(data = filtered_data(), aes(y = EGDI, x = DemIndex + GDPlog, 
-                                                color = EGDILEVEL,label = Country)) +
+                                                color = DemLevel,label = Country)) +
           theme_replace()
         q <- q + geom_point()+
-          scale_color_manual(values = c("Very High EGDI" = "#4682B4", 
-                                        "High EGDI" = "#3CB371", 
-                                        "Middle EGDI" = "#DAA520", "Low EGDI" = "#E9967A"))
+          scale_color_manual(values = c("Full democracy" = "#225ea8",
+                                        "Flawed democracy" = "#1d91c0",
+                                        "Hybrid regime" = "#41b6c4",
+                                        "Authoritarian" = "#7fcdbb",
+                                        "Hybrid regime" = "#41b6c4"))
         
         ggplotly(q)
       })
@@ -75,9 +100,10 @@ shinyApp(
                                                 color = EGDILEVEL,label = Country)) +
           theme_replace()
         r <- r + geom_point()+
-          scale_color_manual(values = c("Very High EGDI" = "#4682B4", 
-                                        "High EGDI" = "#3CB371", 
-                                        "Middle EGDI" = "#DAA520", "Low EGDI" = "#E9967A"))
+          scale_color_manual(values = c("Very High EGDI" = "#225ea8", 
+                                        "High EGDI" = "#1d91c0", 
+                                        "Middle EGDI" = "#41b6c4", 
+                                        "Low EGDI" = "#7fcdbb"))
         
         ggplotly(r)
       })
